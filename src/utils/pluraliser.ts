8eb505c6ab91bf809433b27e8ccedcbdd94a3810
Aaron Carlino <unclecheese@leftandmain.com>
2021-10-07 10:36:45 +1300

    Fix pluraliser bug

--- src/utils/pluraliser.ts
+++ src/utils/pluraliser.ts
@@ -1,7 +1,7 @@
 const defaultPluraliser = (typeName: string): string => {
     let plural = typeName
     if (typeName.match(/[^aeiou]y$/i)) {
-        plural = `${plural.substring(0, -1)}ie`
+        plural = `${plural.slice(0, -1)}ie`
     }
 
     return `${plural}s`
